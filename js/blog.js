28104326cf12467631581d8a07013db680d4880e
mauro pinto <mauroalexanderpinto@gmail.com>
2024-12-06 08:33:40 -0300

    .

--- js/blog.js
+++ js/blog.js
@@ -15,3 +15,12 @@ function leerMas(postId) {
     }
   }
   
+
+  window.addEventListener('scroll', function() {
+    const nav = document.querySelector('.main-nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
